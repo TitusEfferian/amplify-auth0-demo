1d6cf122ebda41c9f2e66fcf7515f876fe92e6f1
titusefferian <tituseff@gmail.com>
2024-05-18 14:11:02 +0900

    init deployment

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from "@aws-amplify/backend";
 
 /**
  * Define and configure your auth resource
@@ -7,5 +7,28 @@ import { defineAuth } from '@aws-amplify/backend';
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      oidc: [
+        {
+          name: "auth0",
+          /**
+           * Since in Amplify, the TypeScript definition of clientId and clientSecret is BackendSecret, 
+           * we need to store the values in Amplify's secret manager.
+           */
+          clientId: secret("authClient"),
+          clientSecret: secret("authSecret"),
+          issuerUrl: "https://testing-amplify.jp.auth0.com",
+          scopes: ["openid", "profile", "email", "name"], // Include the email scope
+        },
+      ],
+      logoutUrls: [
+        "http://localhost:5173",
+        // your staging / production domain later
+      ],
+      callbackUrls: [
+        "http://localhost:5173",
+        // your staging / production domain later
+      ],
+    },
   },
 });
