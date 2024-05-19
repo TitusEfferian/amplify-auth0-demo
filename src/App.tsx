f21c8ff60477498555b7996d3718cd1c77d595d7
titusefferian <tituseff@gmail.com>
2024-05-19 14:21:38 +0900

    gap

--- src/App.tsx
+++ src/App.tsx
@@ -8,63 +8,65 @@ import type { Schema } from "../amplify/data/resource";
 const client = generateClient<Schema>();
 
 function App() {
+
+  const handleSignInWithRedirect = () => {
+    signInWithRedirect({
+      provider: {
+        // use the exact name we defined from the backend configuration
+        custom: "auth0",
+      },
+    });
+  }
+
+  const handleSignOut = () => {
+    signOut({ global: true });
+  }
+  const handleGetCurrentUser = async () => {
+    const session = await fetchAuthSession();
+    console.log(session);
+  }
+  const handleCreateData = async () => {
+    const data = await client.models.TestingTable.create(
+      {
+        content: "hello world",
+      },
+      {
+        // need to specify userPool because we are using owner authorization for this model
+        authMode: "userPool",
+      },
+    );
+    console.log(data);
+  }
+  const handleGetAllData = async () => {
+    // need to specify userPool because we are using owner authorization for this model
+    const data = await client.models.TestingTable.list({ authMode: "userPool" });
+    console.log(data);
+  }
   return (
     <main>
       <button
-        onClick={() => {
-          signInWithRedirect({
-            provider: {
-              custom: "auth0",
-            },
-          });
-        }}
+        onClick={handleSignInWithRedirect}
       >
         signin
       </button>
       <button
-        onClick={async () => {
-          signOut({ global: true });
-        }}
+        onClick={handleSignOut}
       >
         signout
       </button>
       <button
-        onClick={() => {
-          fetchAuthSession().then((x) => {
-            console.log(x);
-          });
-        }}
+        onClick={handleGetCurrentUser}
       >
         get current user
       </button>
 
       <button
-        onClick={async () => {
-          const session = await fetchAuthSession();
-          console.log(session);
-          client.models.TestingTable.create(
-            {
-              content: "hello world",
-
-            },
-            {
-              authMode: "userPool",
-            },
-          ).then((x) => {
-            console.log(x);
-          });
-        }}
+        onClick={handleCreateData}
       >
         create todo
       </button>
       <button
-        onClick={() => {
-          client.models.TestingTable.list({ authMode: "userPool" }).then(
-            (x) => {
-              console.log(x);
-            },
-          );
-        }}
+        onClick={handleGetAllData}
       >
         get data
       </button>
